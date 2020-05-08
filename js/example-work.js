7344c69af0c0fe49d77b0eaa608e94b6dbee7bc7
Robert Litchfield <rl86@evansville.edu>
2020-05-08 13:30:18 -0500

    Adding react code to example work

--- js/example-work.js
+++ js/example-work.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 class ExampleWork extends React.Component {
   render() {
-    <section>It works</section>
+    return(
+      <section className="section section--alignCentered section--description">
+
+      { this.props.work.map( (example, idx) => {
+          return (
+          <ExampleWorkBubble example ={example} key={idx}/>
+        )
+      })}
+
+      </section>
+    )
+  }
+}
+
+class ExampleWorkBubble extends React.Component {
+  render() {
+    let example = this.props.example;
+    return(
+      <div class="section__exampleWrapper">
+        <div class="section__example">
+          <img alt={example.image.desc }
+               class="section__exampleImage"
+               src={example.image.src}/>
+          <dl class="color--cloud">
+            <dt class="section__exampleTitle section__text--centered">
+              {example.title}
+            </dt>
+            <dd></dd>
+          </dl>
+        </div>
+      </div>
+    )
   }
 }
 
